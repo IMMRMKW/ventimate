7cde09526effbcfd43b4e75ba59647ce9f898b0a
IMMRMKW <marcel_welleweerd@hotmail.com>
2024-02-06 22:28:24 +0100

    try

--- components/my_component1/__init__.py
+++ components/my_component1/__init__.py
@@ -14,6 +14,6 @@ CONFIG_SCHEMA = cv.Schema({
 })
 
 def to_code(config):
-	var = cg.new_Pvariable(config[my_component_ns.MyComponent])
-	cg.add(var.set_update_interval(config['update_interval']))
-	cg.add(var.set_iaq_accuracy(config['iaq_accuracy']))
+  var = cg.new_Pvariable(config[my_component_ns.MyComponent])
+  cg.add(var.set_update_interval(config['update_interval']))
+  cg.add(var.set_iaq_accuracy(config['iaq_accuracy']))
