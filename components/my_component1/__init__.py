d62e2a7df088de6c2bdec88e39402ff651dc5713
IMMRMKW <marcel_welleweerd@hotmail.com>
2024-02-09 12:41:57 +0100

    <test>

--- components/my_component1/__init__.py
+++ components/my_component1/__init__.py
@@ -1,23 +1,2 @@
 # my_components/my_component1/__init__.py
 
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import (
-    CONF_ID,
-    CONF_INTERRUPT_PIN,
-)
-
-my_component_ns = cg.esphome_ns.namespace('my_component1')
-MyComponent = my_component_ns.class_('MyComponent', cg.Component)
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MyComponent),
-    cv.Required('iaq_accuracy'): cv.templatable(cv.float_),
-
-}).extend(cv.polling_component_schema("1s"))
-
-async def to_code(config):
-  var = cg.new_Pvariable(config[CONF_ID])
-  await cg.register_component(var, config)
-  cg.add(var.set_iaq_accuracy(10.0))
--- components/my_component1/my_component1.h
+++ components/my_component1/my_component1.h
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include "esphome/components/ssd1331_base/ssd1331_spi.h"
 #include "esphome/core/component.h"
 #include "esphome/core/gpio.h"
 #include "esphome/core/log.h"
@@ -18,41 +19,16 @@
 namespace esphome {
 namespace my_component1 {
 
-    class MyComponent : public Component {
+    class MyComponent : public ssd1331_spi::SPISSD1331,
+                        public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_HIGH, spi::CLOCK_PHASE_TRAILING,
+                            spi::DATA_RATE_8MHZ> {
     public:
-        uint8_t _level = 0;
         // esphome::api::LambdaAction my_variable_setter;
         // float iaq_accuracy = 0.0;
 
-        void
-        setup() override
-        {
-            // my_variable_setter = esphome::api::LambdaAction(this {
-            //     iaq_accuracy = value;
-            // });
-            // this->add_action(my_variable_setter);
-            //  This will be called once to set up the component
-            //  Think of it as the `setup()` call in Arduino
-        }
-
-        void loop() override
-        {
-            const int32_t now = millis();
-            this->last_update_ = now;
-            if (now - this->last_update_ > this->update_interval_) {
-                ESP_LOGD("custom", "The value of sensor is: %f", _level);
-            }
-        }
-        void set_iaq_accuracy(uint8_t level)
-        {
-            _level = level;
-        }
-
-        void set_update_interval(uint32_t val) { update_interval_ = val; };
+        void setup() override;
 
     protected:
-        uint32_t update_interval_ { 0 };
-        uint32_t last_update_ { 0 };
     };
 
 #endif // MY_CUSTOM_COMPONENT_H
