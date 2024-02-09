e7773e63074909fa239fc02cd73bc51f11b34dbc
IMMRMKW <marcel_welleweerd@hotmail.com>
2024-02-09 11:02:57 +0100

    <test>

--- components/my_component1/__init__.py
+++ components/my_component1/__init__.py
@@ -14,12 +14,12 @@ MyComponent = my_component_ns.class_('MyComponent', cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MyComponent),
     cv.Required('iaq_accuracy'): cv.templatable(cv.float_),
-    cv.Required('lambda'): cv.Lambda,
+    
     cv.Optional('update_interval', default='120s'): cv.positive_time_period_milliseconds,
 
 })
 
 def to_code(config):
   var = cg.new_Pvariable(config[CONF_ID])
-  await cg.register_component(var, config)
+  #await cg.register_component(var, config)
   cg.add(var.set_iaq_accuracy(config['iaq_accuracy']))
