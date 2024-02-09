845c6151f4f232dde304e6a75deea925a9cbc1c3
IMMRMKW <marcel_welleweerd@hotmail.com>
2024-02-09 13:42:34 +0100

    <test>

--- components/my_component1/my_component1.cpp
+++ components/my_component1/my_component1.cpp
@@ -2,17 +2,19 @@
 #include "esphome/core/application.h"
 #include "esphome/core/log.h"
 
-static const char* const TAG = "my_component1";
+namespace esphome {
+namespace my_component1 {
+    static const char* const TAG = "my_component1";
 
-void MyComponent::setup()
-{
-    ESP_LOGCONFIG(TAG, "Setting up SPI SSD1331...");
-    this->spi_setup();
-    this->dc_pin_->setup(); // OUTPUT
-    if (this->cs_)
-        this->cs_->setup(); // OUTPUT
+    void MyComponent::setup()
+    {
+        ESP_LOGCONFIG(TAG, "Setting up SPI SSD1331...");
+        this->spi_setup();
+        this->dc_pin_->setup(); // OUTPUT
+        if (this->cs_)
+            this->cs_->setup(); // OUTPUT
 
-    this->init_reset_();
-    delay(500); // NOLINT
-    SSD1331::setup();
-}
+        this->init_reset_();
+        delay(500); // NOLINT
+        SSD1331::setup();
+    }
