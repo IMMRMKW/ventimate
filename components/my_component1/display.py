7058a1d3a7efb52c8b1041d25bbc59c09938dfce
IMMRMKW <marcel_welleweerd@hotmail.com>
2024-02-09 13:03:59 +0100

    <test>

--- components/my_component1/display.py
+++ components/my_component1/display.py
@@ -2,7 +2,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
-from esphome.components import spi, ssd1331_base, ssd1331_spi
+from esphome.components import display, spi, ssd1331_base, ssd1331_spi
 from esphome.const import CONF_DC_PIN, CONF_ID, CONF_LAMBDA, CONF_PAGES
 
 CODEOWNERS = ["@kbx81"]
@@ -15,6 +15,15 @@ ssd1331_spi = cg.esphome_ns.namespace("my_component1")
 MyComponent = ssd1331_spi.class_("MyComponent", ssd1331_base.SSD1331, spi.SPIDevice)
 
 CONFIG_SCHEMA = cv.All(
+    display.FULL_DISPLAY_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(MyComponent),
+            
+        }
+    )
+    .extend(cv.polling_component_schema("5s")),
+    cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA),
+    cv.only_with_arduino,
     ssd1331_base.SSD1331_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(MyComponent),
