272166d72cdc4b85c5652c2537579b6b1178a960
IMMRMKW <marcel_welleweerd@hotmail.com>
2024-02-09 10:27:23 +0100

    <test>

--- components/my_component1/my_component1.h
+++ components/my_component1/my_component1.h
@@ -31,7 +31,7 @@ namespace my_component1 {
         {
             // This will be called very often after setup time
             // Think of it as the `loop()` call in Arduino
-            delay(1000);
+            delay(10);
             ESP_LOGD("custom", "The value of sensor is: %f", _level);
         }
         void set_iaq_accuracy(uint8_t level)
