2398acefd387bb9bd2d6f7c1066d694dc90b870c
IMMRMKW <marcel_welleweerd@hotmail.com>
2024-02-09 11:33:07 +0100

    <test>

--- components/my_component1/__init__.py
+++ components/my_component1/__init__.py
@@ -20,4 +20,4 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
   var = cg.new_Pvariable(config[CONF_ID])
   await cg.register_component(var, config)
-  cg.add(var.set_iaq_accuracy(config['iaq_accuracy']))
+  cg.add(var.set_iaq_accuracy(10.0))
