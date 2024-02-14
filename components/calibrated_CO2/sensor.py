ab80e93dec0dc6ff457d3bf65703c52e457c0b3b
IMMRMKW <marcel_welleweerd@hotmail.com>
2024-02-14 22:07:53 +0100

    <test>

--- components/calibrated_CO2/sensor.py
+++ components/calibrated_CO2/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY
+from esphome.const import CONF_ID,  CONF_SENSOR
 
 calibrated_CO2_ns = cg.esphome_ns.namespace('calibrated_CO2')
 
